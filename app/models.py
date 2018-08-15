4b7c4548ba1a21f145244fbdc960b51a3087d223
hanyiwen <hanyiwen@tju.edu.cn>
2018-08-16 05:35:09 +0800

    update

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 # app/models.py: Posts database table and relationship
 
 from datetime import datetime
+from flask_login import UserMixin
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -13,6 +16,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -23,3 +32,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
